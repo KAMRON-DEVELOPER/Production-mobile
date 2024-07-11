d04511b2a44b11340c10897ce3bc80a29eb13bad
KAMRON-DEVELOPER <atajanovkamronbek2003@gmail.com>
2024-07-11 19:12:12 +0500

    first commit

--- lib/widgets/settings_bar.dart
+++ lib/widgets/settings_bar.dart
@@ -1,3 +1,4 @@
+import 'dart:ui';
 import 'package:flutter/material.dart';
 
 class SettingsBar extends StatelessWidget {
@@ -9,25 +10,28 @@ class SettingsBar extends StatelessWidget {
       bottom: 0,
       left: 0,
       right: 0,
-      child: Container(
-        margin: const EdgeInsets.all(8.0),
-        padding: const EdgeInsets.all(16.0),
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(12.0),
-          boxShadow: const [
-            BoxShadow(
-              color: Colors.black26,
-              blurRadius: 10.0,
-            ),
-          ],
-        ),
-        child: const Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Text('Settings Menu', style: TextStyle(fontSize: 20.0)),
-            // Add your menu items here
-          ],
+      child: BackdropFilter(
+        filter: ImageFilter.blur(sigmaX: 3, sigmaY: 4),
+        child: Container(
+          margin: const EdgeInsets.all(8.0),
+          padding: const EdgeInsets.all(16.0),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(12.0),
+            boxShadow: const [
+              BoxShadow(
+                color: Colors.black26,
+                blurRadius: 10.0,
+              ),
+            ],
+          ),
+          child: const Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text('Settings Menu', style: TextStyle(fontSize: 20.0)),
+              // Add your menu items here
+            ],
+          ),
         ),
       ),
     );
--- lib/widgets/tabbar.dart
+++ lib/widgets/tabbar.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class CustomTabBar extends StatelessWidget {
@@ -12,27 +14,32 @@ class CustomTabBar extends StatelessWidget {
       left: 0,
       right: 0,
       child: Container(
-        color: Colors.blueAccent,
+        height: 40,
         margin: const EdgeInsets.only(left: 8, right: 8),
-        padding: const EdgeInsets.all(8.0),
-        child: const SingleChildScrollView(
-          scrollDirection: Axis.horizontal,
-          child: Row(
-            children: [
-              SizedBox(width: 10),
-              Text('this is tab1'),
-              SizedBox(width: 20),
-              Text('this is tab2'),
-              SizedBox(width: 20),
-              Text('this is tab3'),
-              SizedBox(width: 20),
-              Text('this is tab4'),
-              SizedBox(width: 20),
-              Text('this is tab5'),
-              SizedBox(width: 20),
-              Text('this is tab6'),
-              SizedBox(width: 10),
-            ],
+        decoration: BoxDecoration(
+            border: Border.all(color: Colors.yellow.withOpacity(0.1))),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(6),
+          child: BackdropFilter(
+            filter: ImageFilter.blur(sigmaY: 2, sigmaX: 2),
+            child: Container(
+              color: Colors.transparent,
+              child: ListView.builder(
+                padding: const EdgeInsets.all(4),
+                scrollDirection: Axis.horizontal,
+                itemCount: 5,
+                itemBuilder: (context, index) => GestureDetector(
+                  onTap: () => print('INDEX: $index'),
+                  child: Container(
+                    margin: const EdgeInsets.only(right: 4),
+                    alignment: Alignment.center,
+                    color: Colors.deepOrangeAccent,
+                    padding: const EdgeInsets.fromLTRB(4, 2, 4, 2),
+                    child: Text('dashboard $index'),
+                  ),
+                ),
+              ),
+            ),
           ),
         ),
       ),
